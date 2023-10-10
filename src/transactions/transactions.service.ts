0a090855608e0225edbed2c649bbb76d9b14c854
Diego <diegouvillalobos@gmail.com>
2023-10-10 17:02:00 -0600

    Restore transactions

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,7 +1,7 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Transaction, TransactionDocument } from './transaction.schema';
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class TransactionsService {
@@ -13,7 +13,7 @@ export class TransactionsService {
     return this.transactionModel
       .find({
         userId,
-        type: { $ne: 'restore' },
+        type: 'purchase',
       })
       .exec();
   }
